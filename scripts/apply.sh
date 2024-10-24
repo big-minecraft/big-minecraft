310e9d1f48ead259665946618d0e87dc2d0a2651
wiji <wijigaming.yt@gmail.com>
2024-10-24 00:56:54 -0700

    add supporting features for panel

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,17 +1,17 @@
-command_exists() {
-    command -v "$1" &> /dev/null
-}
-
-if ! command_exists helm; then
-    echo "Helm is not installed. Please install it by following the instructions at https://helm.sh/docs/intro/install/"
-    exit 1
-fi
-
-if ! command_exists helmfile; then
-    echo "Helmfile is not installed. Please install it by following the instructions at https://github.com/roboll/helmfile#installation"
-    exit 1
-fi
-
-
-helm upgrade big-minecraft ../
-helmfile apply --file ../helmfile.yaml  
+    command_exists() {
+        command -v "$1" &> /dev/null
+    }
+    
+    if ! command_exists helm; then
+        echo "Helm is not installed. Please install it by following the instructions at https://helm.sh/docs/intro/install/"
+        exit 1
+    fi
+    
+    if ! command_exists helmfile; then
+        echo "Helmfile is not installed. Please install it by following the instructions at https://github.com/roboll/helmfile#installation"
+        exit 1
+    fi
+    
+    
+    helm upgrade big-minecraft ../
+    helmfile apply --file ../helmfile.yaml  
