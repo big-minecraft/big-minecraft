4d3c76360284c7f13e001beb78d26e60192e8b8c
Timothy <timothyrwashburn@gmail.com>
2024-11-10 12:25:32 -0800

    display download info + fix kubectl version check

--- scripts/install-dependents.sh
+++ scripts/install-dependents.sh
@@ -17,7 +17,7 @@ echo "Starting installation of helm tools and kubectl"
 # Install curl
 if ! command_exists curl; then
     echo "Installing curl"
-    wget -q "https://github.com/moparisthebest/static-curl/releases/download/${CURL_VERSION}/curl-amd64" -O /usr/local/bin/curl
+    wget "https://github.com/moparisthebest/static-curl/releases/download/${CURL_VERSION}/curl-amd64" -O /usr/local/bin/curl
     chmod +x /usr/local/bin/curl
     export PATH="/usr/local/bin:$PATH"
 fi
@@ -33,7 +33,7 @@ fi
 # Install helm
 if ! command_exists helm; then
     echo "Installing helm"
-    curl -sL "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" | tar xz
+    curl -L "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" | tar xz
     mv linux-amd64/helm /usr/local/bin/
     rm -rf linux-amd64
 fi
@@ -43,7 +43,7 @@ if ! command_exists helmfile; then
     echo "Installing helmfile"
     TMP_DIR=$(mktemp -d)
     cd "$TMP_DIR"
-    curl -sL "https://github.com/helmfile/helmfile/releases/download/${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION#v}_linux_amd64.tar.gz" | tar xz
+    curl -L "https://github.com/helmfile/helmfile/releases/download/${HELMFILE_VERSION}/helmfile_${HELMFILE_VERSION#v}_linux_amd64.tar.gz" | tar xz
     chmod +x helmfile
     mv helmfile /usr/local/bin/
     cd - > /dev/null
@@ -55,12 +55,12 @@ if ! helm plugin list | grep -q "diff"; then
     echo "Installing helm-diff plugin"
     PLUGIN_DIR="${HELM_PLUGIN_DIR:-$HOME/.local/share/helm/plugins}"
     mkdir -p "$PLUGIN_DIR"
-    curl -sL "https://github.com/databus23/helm-diff/releases/download/${HELM_DIFF_VERSION}/helm-diff-linux-amd64.tgz" | tar xz -C "$PLUGIN_DIR"
+    curl -L "https://github.com/databus23/helm-diff/releases/download/${HELM_DIFF_VERSION}/helm-diff-linux-amd64.tgz" | tar xz -C "$PLUGIN_DIR"
 fi
 
 # Verify installations
 echo -e "\nVerifying installations:"
-echo "Kubectl: $(kubectl version --client -o json | grep -o '"gitVersion":"[^"]*' | cut -d':' -f2 | tr -d '"')"
+echo "Kubectl: $(kubectl version --client | grep 'Client Version:' | cut -d' ' -f3)"
 echo "Helm: $(helm version --short)"
 echo "Helmfile: $(helmfile -v)"
 echo "Helm-diff: $(helm plugin list | grep diff)"
